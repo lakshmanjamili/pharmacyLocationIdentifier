bc5a6e1a36964fb2ddafd7c65dec1384eabb770a
lakshman jamili <lj062465@cerner.net>
2021-08-31 08:39:20 -0500

    First commit

---
+++ src/main/java/com/rxsolutions/pharmancyLocationIndentifier/PharmancyLocationIndentifierApplication.java
@@ -0,0 +1,13 @@
+package com.rxsolutions.pharmancyLocationIndentifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmancyLocationIndentifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmancyLocationIndentifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rxsolutions/pharmancyLocationIndentifier/PharmancyLocationIndentifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxsolutions.pharmancyLocationIndentifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmancyLocationIndentifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
